45de02db0afc7052aac046076256b931203f6553
Jackson Grimm <92557509+Bdogy@users.noreply.github.com>
2022-12-02 15:58:40 -0700

    Added db structure

---
+++ db/schema.sql
@@ -0,0 +1,29 @@
+DROP DATABASE IF EXISTS company_db;
+CREATE DATABASE company_db;
+
+USE company_db;
+
+CREATE TABLE department(
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30)
+);
+
+CREATE TABLE role(
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_id INT
+);
+
+CREATE TABLE employee(
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	first_name VARCHAR(30),
+	last_name VARCHAR(30),
+    role_id INT,
+    mananger_id INT
+);
+
+
+SELECT * FROM department;
+SELECT * FROM role;
+SELECT * FROM employee;
