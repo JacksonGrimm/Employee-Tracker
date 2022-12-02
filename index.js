d7de067cc33c8c339a0b04be45227e1d5ef9f55c
Jackson Grimm <92557509+Bdogy@users.noreply.github.com>
2022-12-02 16:17:14 -0700

    set up file structure and linked db

---
+++ index.js
@@ -0,0 +1,13 @@
+const mysql = require("mysql2");
+//dataBase connection
+const db = mysql.createConnection(
+  {
+    host: "localhost",
+    // MySQL username,
+    user: "root",
+    // MySQL password
+    password: "root",
+    database: "company_db",
+  },
+  console.log(`Connected to the company_db; database.`)
+);
